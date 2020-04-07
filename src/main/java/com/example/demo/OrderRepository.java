35cd63a64f05e5acf6ff131932ff1a6d51d71120
SKCC <SKCC@SKCC18D00112.SKCC.NET>
2020-04-07 17:09:11 +0900

    first commit

---
+++ src/main/java/com/example/demo/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+import com.example.demo.Order;
+import org.springframework.data.repository.CrudRepository;
+
+public interface OrderRepository extends CrudRepository<Order, Long>{
+}
